c28c1b5a731f1687aed8ac3a4b37154a8ad777f2
Vachan DA <vachan.da@urbanladder.com>
2015-12-08 12:44:52 +0530

    Added the method to fetch from mongo and the a route to fetch

--- live_feed/mongo_doc.py
+++ live_feed/mongo_doc.py
@@ -9,7 +9,7 @@ class MongoDoc(object):
     def read_properties(self, setting_type, setting):
         parser = SafeConfigParser()
         try:
-            parser.read("application.properties")
+            parser.read("social_media.config")
             return parser.get(setting_type, setting)
         except (ConfigParser.ParsingError, ConfigParser.NoSectionError) as err:
             print "Error while reading mongo settings from file. {}".format(err)
@@ -33,3 +33,23 @@ class MongoOP(MongoDoc):
             db_conn.insert_one(feed)
         except (pymongo.errors.WriteError, pymongo.errors.CollectionInvalid) as err:
             print "Error writing value {} to collection {}. {}".format(feed, collection, err)
+
+    def fetch_feed(self, fetch_param):
+        db_conn = self.client
+        collection = 'feed'
+        try:
+            db_conn = db_conn[collection]
+            if fetch_param is None:
+                cursor = db_conn.find().sort('created_at', pymongo.DESCENDING).limit(50)
+            else:
+                param = {'created_at': { '$gt': str(fetch_param)}}
+                cursor = db_conn.find(param).sort('created_at', pymongo.DESCENDING)
+            feed = {}
+            index = 1
+            for doc in cursor:
+                feed[index] = doc
+                feed[index].pop('_id')
+                index += 1
+            return feed
+        except pymongo.errors.PyMongoError as err:
+            print "Error fetching documents from collection {}. {}".format(collection, err)
